6422bf8a314c0f0dc25277f36b88e9713d13e530
Ashley Rarieya <ararieya@gmail.com>
2023-10-16 14:57:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const length = str.length;
+  
+  for (let i = 0; i < length / 2; i++) {
+    if (str[i] !== str[length - 1 - i]) {
+      return false;
+    }
+  }
+  
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
